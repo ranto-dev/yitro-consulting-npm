ea4de5205c540ff52ec21c06696b7fb1b5484a26
ranto-dev <rantoandrianandraina@gmail.com>
2025-09-22 14:34:18 +0200

    configuration du react-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,30 @@
-import { useState, useEffect } from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Services from "./components/Services";
-import IntroVideo from "./components/IntroVideo";
-import Portfolio from "./components/Portfolio";
-import ScrollToTopButton from "./components/ScrollToTopButton";
-import Pricing from "./components/Pricing";
-import CallToAction from "./components/CallToAction";
-import Team from "./components/Team";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import Blog from "./components/Blog";
-// import Testimonials from "./components/Testimonials";
+import Blog from "./pages/client/Blog";
+import Home from "./pages/client/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-function App() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 1000);
-    return () => clearTimeout(timer);
-  }, []);
+export default function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <>
+          <Home />
+        </>
+      ),
+    },
+    {
+      path: "/blog",
+      element: (
+        <>
+          <Blog />
+        </>
+      ),
+    },
+  ]);
 
   return (
     <>
-      {isLoading && (
-        <div
-          className="page-loading fixed top-0 bottom-0 left-0 right-0 z-[99999] flex items-center justify-center bg-primary-light-1 dark:bg-primary-dark-1 opacity-100 visible pointer-events-auto"
-          role="status"
-          aria-live="polite"
-          aria-atomic="true"
-          aria-label="Loading..."
-        >
-          <div className="grid-loader">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-        </div>
-      )}
-
-      <Navbar />
-      <div className="main relative">
-        <Hero />
-        <About />
-        <Services />
-        <IntroVideo />
-        <Portfolio />
-        <Pricing />
-        <CallToAction />
-        <Team />
-        {/* <Testimonials /> */}
-        <Blog />
-        <Contact />
-      </div>
-      <Footer />
-      <ScrollToTopButton />
+      <RouterProvider router={router}></RouterProvider>
     </>
   );
 }
-
-export default App;
---
+++ src/pages/client/Home.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import Navbar from "../../components/Navbar";
+import Hero from "../../components/Hero";
+import About from "../../components/About";
+import Services from "../../components/Services";
+import IntroVideo from "../../components/IntroVideo";
+import Portfolio from "../../components/Portfolio";
+import Pricing from "../../components/Pricing";
+import CallToAction from "../../components/CallToAction";
+import Team from "../../components/Team";
+import Blog from "../../components/Blog";
+import Contact from "../../components/Contact";
+import Footer from "../../components/Footer";
+import ScrollToTopButton from "../../components/ScrollToTopButton";
+
+export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      {isLoading && (
+        <div
+          className="page-loading fixed top-0 bottom-0 left-0 right-0 z-[99999] flex items-center justify-center bg-primary-light-1 dark:bg-primary-dark-1 opacity-100 visible pointer-events-auto"
+          role="status"
+          aria-live="polite"
+          aria-atomic="true"
+          aria-label="Loading..."
+        >
+          <div className="grid-loader">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      )}
+
+      <Navbar />
+      <div className="main relative">
+        <Hero />
+        <About />
+        <Services />
+        <IntroVideo />
+        <Portfolio />
+        <Pricing />
+        <CallToAction />
+        <Team />
+        <Blog />
+        <Contact />
+      </div>
+      <Footer />
+      <ScrollToTopButton />
+    </>
+  );
+}
