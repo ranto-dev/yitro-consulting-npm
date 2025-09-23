534e048a4e558bd63a8416de03834e69548f2cf3
MaminirinaEdwino <edwinomaminirina@gmail.com>
2025-09-23 13:41:03 +0300

    traduction

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,8 +1,14 @@
 import heroImage from "../../assets/img/hero.jpg";
 import dots from "../../assets/img/dots.svg";
 import { FaPlay } from "react-icons/fa6";
+import traduction from "../../../public/traduction/traduction.json";
 
-const Hero = () => {
+interface lang {
+  lang: string;
+}
+
+const Hero = ({ lang }: lang) => {
+  
   return (
     <section
       id="home"
@@ -13,13 +19,13 @@ const Hero = () => {
           <div className="w-full px-5">
             <div className="scroll-revealed mx-auto max-w-[780px] text-center">
               <h1 className="mb-6 text-3xl font-bold leading-snug text-primary-color sm:text-4xl sm:leading-snug lg:text-5xl lg:leading-tight">
-                Welcome to Yitro-consulting
+                
+                {lang == "eng" && traduction["eng"]["heroTitle"]}
+                {lang == "fr" && traduction["fr"]["heroTitle"]}
               </h1>
               <p className="mx-auto mb-9 max-w-[600px] text-base text-primary-color sm:text-lg sm:leading-normal">
-                Yitro-Consulting turns your ambitions into digital reality.
-                Tailor-made solutions that boost your visibility, engage your
-                clients, and drive your sales. From audit to delivery, we
-                support every step of your digital transformation.
+                {lang == "eng" && traduction["eng"]["paragraphe"]}
+                {lang == "fr" && traduction["fr"]["paragraphe"]}
               </p>
               <ul className="mb-10 flex flex-wrap items-center justify-center gap-4 md:gap-5">
                 <li>
@@ -28,7 +34,8 @@ const Hero = () => {
                     className="inline-flex items-center justify-center rounded-md bg-primary-color text-primary px-5 py-3 text-center text-base font-medium shadow-md hover:bg-primary-light-5 md:px-7 md:py-[14px]"
                     role="button"
                   >
-                    Get Started
+                    {lang == "eng" && traduction["eng"]["get_started"]}
+                    {lang == "fr" && traduction["fr"]["get_started"]}
                   </a>
                 </li>
                 <li>
@@ -37,7 +44,9 @@ const Hero = () => {
                     className="video-popup flex items-center gap-4 rounded-md bg-primary-color/[0.15] px-5 py-3 text-base font-medium text-primary-color hover:bg-primary-color hover:text-primary md:px-7 md:py-[14px]"
                     role="button"
                   >
-                    <FaPlay /> Watch Intro
+                    <FaPlay /> 
+                    {lang == "eng" && traduction["eng"]["watch_intro"]}
+                    {lang == "fr" && traduction["fr"]["watch_intro"]}
                   </a>
                 </li>
               </ul>
