14856dbfbb68cc2a343081fd3f7ab38dcdfa2deb
ranto-dev <rantoandrianandraina@gmail.com>
2025-09-27 15:19:40 +0200

    bg color to navbar, dashboard section

--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -4,19 +4,22 @@ import Content from "../../components/blog/Content";
 export default function Dashboard() {
   return (
     <div className="w-full">
-      <div className="flex justify-between items-center p-4 sticky top-0 shadow-[2rem] " style={{ borderBottom: "1rem" }}>
+      <div
+        className="flex justify-between items-center bg-primary p-4 sticky top-0 shadow-[2rem]"
+        style={{ borderBottom: "1rem" }}
+      >
         <div>
-          <h1 className="text-sm">Blog Admin</h1>
+          <h1 className="text-sm text-white">Blog Admin</h1>
         </div>
         <div>
-          <button className="flex justify-center items-center gap-2 bg-primary overflow-hidden rounded-full p-4 text-white text-sm">
+          <button className="flex justify-center items-center gap-2 border border-white rounded-full p-4 text-white text-sm">
             <FaPlusCircle />
             New Post
           </button>
         </div>
       </div>
       <div>
-        <div className="text-center">
+        <div className="text-center my-10">
           <h1>All Posts</h1>
           <Content />
         </div>
